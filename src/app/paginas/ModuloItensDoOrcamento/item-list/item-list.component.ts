f154ac0641ab6f1383ce19d17d3a08bb23aad15b
Raphael Rodrigues <73262792+raphadvlp@users.noreply.github.com>
2025-03-27 13:10:44 -0300

    feat: formação modeda para pt-br / Validação de apenas 1 item com periodo anual chore: mudança api para bscar da environmet;

--- src/app/paginas/ModuloItensDoOrcamento/item-list/item-list.component.ts
+++ src/app/paginas/ModuloItensDoOrcamento/item-list/item-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
@@ -11,6 +11,8 @@ import {
   PoFieldModule,
 } from '@po-ui/ng-components';
 import { environment } from '../../../../environments/environment';
+
+import { BrCurrencyPipe } from './br-currency.pipe';
 
 @Component({
   selector: 'app-item-list',
@@ -28,9 +30,13 @@ import { environment } from '../../../../environments/environment';
   ],
 })
 export class ItemListComponent implements OnInit {
-  lookupServiceContaOrcamentaria =
-    'http://localhost:8000/api/mock/contaorcamentaria';
-  lookupServiceCentroDeCusto = 'http://localhost:8000/api/mock/centrodecusto';
+
+  lookupServiceContaOrcamentaria = `${environment.url}/api/mock/contaorcamentaria`;
+  lookupServiceCentroDeCusto = `${environment.url}/api/mock/centrodecusto`;
+
+  // lookupServiceContaOrcamentaria =
+  //   'http://localhost:8000/api/mock/contaorcamentaria';
+  // lookupServiceCentroDeCusto = 'http://localhost:8000/api/mock/centrodecusto';
 
   // Campos do formulário dinâmico
   fields: PoDynamicFormField[] = [
@@ -52,6 +58,7 @@ export class ItemListComponent implements OnInit {
       gridColumns: 2,
       placeholder: 'Código Orçamento',
       noAutocomplete: true,
+      disabled: false,
     },
     {
       property: 'conta',
@@ -59,6 +66,7 @@ export class ItemListComponent implements OnInit {
       type: 'string',
       required: true,
       gridColumns: 2,
+      disabled: false,
       searchService: this.lookupServiceContaOrcamentaria,
       fieldLabel: 'nome_contaorcamentaria', // Exibe apenas o código
       fieldValue: 'nome_contaorcamentaria', // Valor retornado ao selecionar
@@ -74,6 +82,7 @@ export class ItemListComponent implements OnInit {
       label: 'Centro de Custo',
       type: 'string',
       required: true,
+      disabled: false,
       gridColumns: 2,
       searchService: this.lookupServiceCentroDeCusto,
       fieldLabel: 'codigo', // Nome da propriedade que será exibida no lookup
@@ -91,6 +100,7 @@ export class ItemListComponent implements OnInit {
       type: 'date',
       format: 'dd/MM/yyyy',
       required: true,
+      disabled: false,
       gridColumns: 2,
       placeholder: '00/00/0000',
       noAutocomplete: true,
@@ -100,12 +110,18 @@ export class ItemListComponent implements OnInit {
       label: 'Valor',
       type: 'number',
       required: true,
+      disabled: false,
       gridColumns: 2,
       // placeholder: '0,00',
       clean: true,
       noAutocomplete: true,
+      decimalsLength: 2, // Duas casas decimais
+      thousandMaxlength: 3, // Separador de milhar
+      icon: 'po-icon-money' // Ícone de moeda
     },
   ];
+
+
 
   // Valores do formulário
   formValues: any = {};
@@ -120,6 +136,11 @@ export class ItemListComponent implements OnInit {
   // Contador de sequência de itens
   itemSequence: number = 1;
 
+  // Propriedade para receber o período
+  @Input() periodo: string = 'Mensal';
+
+  
+
   // Colunas da tabela
   columns = [
     { property: 'item', label: 'Item' },
@@ -127,7 +148,17 @@ export class ItemListComponent implements OnInit {
     { property: 'conta', label: 'Conta Orçamentária' },
     { property: 'cc', label: 'Centro de Custo' },
     { property: 'dataP', label: 'Data', type: 'date', format: 'dd/MM/yyyy' },
-    { property: 'valor', label: 'Valor', type: 'number' },
+    { 
+      property: 'valor', 
+      label: 'Valor', 
+      type: 'number',
+      format: '1.2-2', // Formato Angular - mínimo 1 dígito antes e 2 depois da vírgula
+      // pipe: {
+      //   property: 'valor',
+      //   format: 'BRL' // Formato de moeda brasileira (R$)
+      // }
+      pipe: new BrCurrencyPipe() // Usa o pipe programaticamente
+    },
   ];
 
   // Ações da tabela
@@ -143,6 +174,7 @@ export class ItemListComponent implements OnInit {
       icon: 'po-icon-edit',
     },
   ];
+  
 
   constructor(private http: HttpClient) {}
 
@@ -160,36 +192,81 @@ export class ItemListComponent implements OnInit {
     }
   }
 
+  // Método chamado quando qualquer input mudar
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['periodo']) {
+      this.atualizarEstadoCampos();
+    }
+  }
+
+
+  // Método para atualizar o estado dos campos
+  atualizarEstadoCampos() {
+    const camposParaDesabilitar = ['orcamento', 'conta', 'cc', 'dataP', 'valor'];
+    const deveDesabilitar = this.periodo === 'Anual' && this.items.length >= 1;
+
+    this.fields = this.fields.map(field => {
+      if (camposParaDesabilitar.includes(field.property)) {
+        return {
+          ...field,
+          disabled: deveDesabilitar
+        };
+      }
+      return field;
+    });
+  }
+
   // Adicionar ou salvar item
+  // AdicionarOuSalvarItem para chamando atualizarEstadoCampos
   adicionarOuSalvarItem() {
+    if (this.periodo === 'Anual' && this.items.length >= 1 && this.editingIndex === null) {
+      alert('Período anual permite apenas 1 item. Não é possível adicionar mais itens.');
+      return;
+    }
+  
+    // Formata o valor antes de adicionar
+    const valorFormatado = this.formValues.valor ?
+      parseFloat(this.formValues.valor).toFixed(2) :
+      null;
+  
     if (this.editingIndex !== null) {
-      // Salvar edição
       this.items[this.editingIndex] = {
+        // Salvar edição
         ...this.formValues,
-        actions: ['edit', 'delete'], // Adiciona as ações
+        valor: valorFormatado,
+        // Adiciona as ações
+        actions: ['edit', 'delete'],
       };
       this.editingIndex = null;
     } else {
-      // Adicionar novo item
       this.items.push({
         ...this.formValues,
-        actions: ['edit', 'delete'], // Adiciona as ações
+        valor: valorFormatado,
+        actions: ['edit', 'delete'],
       });
-      this.itemSequence++; // Incrementar a sequência do próximo item
-      this.formValues.item = this.itemSequence; // Atualizar o valor do campo item
-    }
 
-    // Limpa o formulário, exceto o campo item
+      // Incrementar a sequência do próximo item
+      this.itemSequence++;
+
+      // Atualizar o valor do campo item
+      this.formValues.item = this.itemSequence;
+    }
+  
+    // Limpa o formulário
     this.formValues.orcamento = '';
     this.formValues.conta = '';
     this.formValues.cc = '';
     this.formValues.dataP = '';
     this.formValues.valor = null;
+  
+    // Atualiza o estado dos campos após adicionar/editar
+    this.atualizarEstadoCampos();
   }
 
-  // Excluir item
+  // Modifique o excluirItem para chamar atualizarEstadoCampos
   excluirItem(item: any) {
     this.items = this.items.filter((i) => i !== item);
+    this.atualizarEstadoCampos(); // Atualiza o estado dos campos após exclusão
     console.log('Item excluído:', item); // Debug: Verifique se o item foi excluído
   }
 
